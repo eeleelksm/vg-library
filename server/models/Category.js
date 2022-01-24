044043fe74540b8a73354d74238db8f2a1465ada
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-24 17:48:52 -0500

    completed Game model and commented out Category model

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,1 +1,16 @@
 // work on once all other models/routes are working as intended
+// const mongoose = require('mongoose');
+
+// const { Schema } = mongoose;
+
+// const categorySchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   }
+// });
+
+// const Category = mongoose.model('Category', categorySchema);
+
+// module.exports = Category;
--- server/models/Game.js
+++ server/models/Game.js
@@ -1,1 +1,33 @@
-const mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const gameSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	description: {
+		type: String,
+	},
+	system: {
+		type: String,
+	},
+	image: {
+		type: String,
+	},
+	// price: {
+	//   type: Number,
+	//   required: true,
+	//   min: 0.99
+	// },
+	// category: {
+	//   type: Schema.Types.ObjectId,
+	//   ref: "Category",
+	//   required: true
+	// }
+});
+
+const Game = mongoose.model("Game", gameSchema);
+
+module.exports = Game;
