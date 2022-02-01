354f92431ebdaecb95b2101b31aa60f7944b2299
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-02-01 15:48:32 -0500

    npm i apollo client to connect front and back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import {
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+	createHttpLink,
+} from "@apollo/client";
+
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Home from "./pages/Home/Home";
@@ -8,22 +15,32 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import "./App.scss";
 
+const httpLink = createHttpLink({
+	uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 function App() {
 	// the navigation bar opening and closing
 	const [navOpen, setNavOpen] = useState(false);
 
 	return (
-		<div className="app">
-			<Header navOpen={navOpen} setNavOpen={setNavOpen} />
-			<Nav navOpen={navOpen} setNavOpen={setNavOpen} />
-			<div className="sections">
-				<Home />
-				<SearchGames />
-				<SavedGames />
-				<Login />
-				<Signup />
+		<ApolloProvider client={client}>
+			<div className="app">
+				<Header navOpen={navOpen} setNavOpen={setNavOpen} />
+				<Nav navOpen={navOpen} setNavOpen={setNavOpen} />
+				<div className="sections">
+					<Home />
+					<SearchGames />
+					<SavedGames />
+					<Login />
+					<Signup />
+				</div>
 			</div>
-		</div>
+		</ApolloProvider>
 	);
 }
 
